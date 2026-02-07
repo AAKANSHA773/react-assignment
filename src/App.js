455d0e3e1fa48bcf738d0d723bfe064b2d8d0be2
AAKANSHA773 <aakanshapanwar01@gmail.com>
2026-02-07 13:23:01 +0530

    add tailwind css to the assignment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import UserForm from './components/UserFrom';
 
 function App() {
   return (
     <div className="App">
-      <h1>User Mangement dashbord</h1>
+  <h1 className="text-3xl font-bold underline">
+      User Dashboard
+    </h1>
+      <div>
+        <UserForm/>
+      </div>
     </div>
   );
 }
